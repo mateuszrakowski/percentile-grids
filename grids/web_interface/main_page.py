8377acd61c8b0d5545920514b8d6f34071370478
m.rakowski <mrakowski@m.rakowski-B550-AORUS-ELITE-V2>
2025-02-15 21:09:16 +0100

    Fixes for displaying GUI after button is clicked, refactored db_utils

--- grids/web_interface/main_page.py
+++ grids/web_interface/main_page.py
@@ -1,34 +1,41 @@
-import pandas as pd
-import sqlalchemy
+from time import sleep
+
 import streamlit as st
-from src.update_db import table_missing, update_db
+from src.db_utils import load_data, table_missing, update_db
+
+st.title("Welcome to the Percentile Grids Web Interface")
 
 
-def main():
-    st.title("Welcome to the Percentile Grids Web Interface")
+if "uploader_key" not in st.session_state:
+    st.session_state["uploader_key"] = 1
 
-    if table_missing("PatientRecords"):
-        st.warning(
-            "The database is empty. Please upload a CSV files to populate the database."
-        )
-    else:
-        engine = sqlalchemy.create_engine("sqlite:///grids/reference_dataset.db")
-        patient_structures = pd.read_sql("SELECT * FROM PatientStructures", engine)
-        st.write(
-            "Reference patient dataset:",
-            patient_structures,
-            "Number of patients: ",
-            len(patient_structures),
-        )
+current_data = load_data()
 
-    uploaded_files = st.file_uploader(
-        "Choose a CSV files", accept_multiple_files=True, type="csv, xlsx, xls"
+if current_data is None:
+    st.warning(
+        "The database is empty. Please upload a CSV file to populate the database."
     )
-    if st.button("Send data to database."):
-        if not uploaded_files:
-            st.warning("Please select CSV files first.")
-            return
+else:
+    st.write("Reference patient dataset.")
+    st.write("Number of patients:", len(current_data))
+    st.dataframe(current_data)
+
+uploaded_files = st.file_uploader(
+    "Choose a CSV file",
+    accept_multiple_files=True,
+    type=["csv", "xlsx", "xls"],
+    key=st.session_state["uploader_key"],
+)
+
+if st.button("Send data to database"):
+    if not uploaded_files:
+        st.warning("Please select CSV files first.")
+    else:
         update_db(uploaded_files)
+        with st.spinner("Sending data to the database..."):
+            sleep(3)
+        st.success("Data successfully sent to the database!")
+        sleep(1)
 
-
-main()
+        st.session_state["uploader_key"] += 1
+        st.rerun()
--- grids/web_interface/src/db_utils.py
+++ grids/web_interface/src/db_utils.py
@@ -1,21 +1,36 @@
+import os
 import sqlite3
 from io import StringIO
 
 import pandas as pd
+import sqlalchemy
+from sqlalchemy import text
 from src.process_input import process_input
 from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 
+def init_database(name: str = "grids/reference_dataset.db") -> bool:
+    if not os.path.exists(name):
+        conn = sqlite3.connect(name)
+        conn.close()
+
+
 def table_missing(name: str) -> bool:
+    init_database()
+
     con = sqlite3.connect("grids/reference_dataset.db")
     cur = con.cursor()
-    cur.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{name}'")
+
+    cur.execute(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name = :table_name",
+        {"table_name": name},
+    )
     return cur.fetchone() is None
 
 
-def create_tables(
-    cur: sqlite3.Cursor, records: pd.DataFrame, structures: pd.DataFrame
-) -> None:
+def create_tables(cur: sqlite3.Cursor, sample_dataframe: pd.DataFrame) -> None:
+    records, structures = process_input(sample_dataframe)
+
     record_columns = [f"{col} TEXT" for col in records.columns]
     structure_columns = record_columns + [
         f"{col} REAL" for col in structures.columns[4:]
@@ -38,14 +53,20 @@ def convert_to_dataframe(input_files: list[UploadedFile]) -> list[pd.DataFrame]:
     return [pd.read_csv(StringIO(file.read().decode("utf-8"))) for file in input_files]
 
 
+def load_data():
+    if table_missing("PatientStructures") or table_missing("PatientRecords"):
+        return None
+    engine = sqlalchemy.create_engine("sqlite:///grids/reference_dataset.db")
+    return pd.read_sql("SELECT * FROM PatientStructures", engine)
+
+
 def update_db(input_files: list[UploadedFile]) -> None:
     dataframes = convert_to_dataframe(input_files)
 
     con = sqlite3.connect("grids/reference_dataset.db")
     cur = con.cursor()
 
-    records, structures = process_input(dataframes[0])
-    create_tables(cur, records, structures)
+    create_tables(cur, dataframes[0])
 
     records_data = []
     structures_data = []
